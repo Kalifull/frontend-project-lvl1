49127007906bbb08faa44b08cca6a1e7747cba54
Alexey <kalifull@mail.ru>
2022-04-16 23:26:36 +0300

    add new game

---
+++ src/games/brain-gcd.js
@@ -0,0 +1,23 @@
+import games, { getRandomInt } from '../index.js';
+
+const rules = 'Find the greatest common divisor of given numbers.';
+
+const gcd = (num1, num2) => (num2 !== 0 ? gcd(num2, num1 % num2) : num1);
+
+const maxFirstNumber = 100;
+
+const maxSecondNumber = 100;
+
+const getCorrectAnswer = () => {
+  const firstNumber = getRandomInt(0, maxFirstNumber);
+  const secondNumber = getRandomInt(0, maxSecondNumber);
+  const question = `${firstNumber} ${secondNumber}`;
+  const correctAnswer = gcd(firstNumber, secondNumber);
+  return [question, correctAnswer];
+};
+
+const brainGCD = () => {
+  games(rules, getCorrectAnswer);
+};
+
+export default brainGCD;
